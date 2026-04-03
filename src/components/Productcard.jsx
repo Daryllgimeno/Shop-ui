cd378d6e81c7aae79e1bb0b4deeba404747504cc
Daryll Gimeno <75570103+Daryllgimeno@users.noreply.github.com>
2026-04-04 01:39:17 +0800

    Adding

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 function Productcard(props) {
   return (
     <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
-      <h1 className="text-xl font-medium text-black dark:text-whit">Product: {props.product}</h1>
-      <p>Description: {props.description}</p>
-
-      <button onClick={() => props.onAdd(props.product)}>
+      
+      <div className="space-y-2 text-center sm:text-left">
+      <div className='space-y-4'>
+      <h1 className='text-lg font-bold text-black'>Product: {props.product}</h1>
+      <p className='font-medium text-gray-500'>Description: {props.description}</p>
+      </div>
+    </div>
+      <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ..." onClick={() => props.onAdd(props.product)}>
         Buy
       </button>
     </div>
