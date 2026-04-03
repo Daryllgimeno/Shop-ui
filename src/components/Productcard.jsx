346956594df4d077a7b1298a7d01f32857282232
Daryll Gimeno <75570103+Daryllgimeno@users.noreply.github.com>
2026-04-04 00:52:43 +0800

    Addig

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Productcard(props) {
   return (
-    <div>
-      <h1>Product: {props.product}</h1>
+    <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+      <h1 className="text-xl font-medium text-black dark:text-whit">Product: {props.product}</h1>
       <p>Description: {props.description}</p>
 
       <button onClick={() => props.onAdd(props.product)}>
