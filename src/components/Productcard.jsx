03f3bcf5e9d33835300a2d6742ed6241ddee9c90
Daryll Gimeno <75570103+Daryllgimeno@users.noreply.github.com>
2026-04-04 02:17:38 +0800

    adding text

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -4,7 +4,7 @@ function Productcard(props) {
   return (
     <div className="flex flex-col justify-between items-center p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition w-72">
       
-      <div className="space-y-2 text-center">
+      <div className="space-y-3 text-center">
         <h1 className="text-xl font-semibold text-gray-800">
           Product: {props.product}
         </h1>
